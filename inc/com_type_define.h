7b47c5d923838f8a25518adbf27a17f3f731bd79
ArthurWater <1185039101@qq.com>
2018-01-13 19:08:04 +0800

    初步程序头文件

---
+++ inc/com_type_define.h
@@ -0,0 +1,18 @@
+
+#ifndef _COM_TYPE_DEFINE_H_
+#define _COM_TYPE_DEFINE_H_
+
+
+typedef int INT;
+typedef char INT8;
+typedef unsigned int UINT;
+typedef unsigned char UINT8;
+
+
+
+
+
+
+
+#endif
+
---
+++ inc/common.h
@@ -0,0 +1,10 @@
+#ifndef __COMMON__
+#define __COMMON__
+
+#define PRT printf
+#define PRT_ERR printf("error: %s[%d]", __FILE__, __LINE__);printf
+
+#define CNN_LAYER_NUM 5
+#define CNN_MAP_SIZE 5
+#define CNN_LAYER1_MAP_SIZE 5
+#endif
